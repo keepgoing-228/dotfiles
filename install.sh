cc0448963da3dfcffb318b57bf8a311c9ad60751
keepgoing-228 <keepdling@gmail.com>
2025-07-30 01:48:51 +0800

    feat: automating install oh-my-zsh, p10k, zsh extra plugin

--- install.sh
+++ install.sh
@@ -4,6 +4,41 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 DOT_FOLDERS="zsh,p10k,tmux,git"
 
+# install oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "[+] Installing oh-my-zsh..."
+    RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "[✓] oh-my-zsh already installed."
+fi
+
+# install powerlevel10k
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+P10K_DIR="$ZSH_CUSTOM/themes/powerlevel10k"
+
+if [ ! -d "$P10K_DIR" ]; then
+    echo "[+] Installing powerlevel10k theme..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
+else
+    echo "[✓] powerlevel10k already installed."
+fi
+
+# zshrc plugin: zsh-autosuggestions and pwp
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    echo "[+] Installing zsh-autosuggestions plugin..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+else
+    echo "[✓] zsh-autosuggestions already installed."
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/pwp" ]; then
+    echo "[+] Installing pwp plugin..."
+    git clone https://github.com/ttkalcevic/pwp.git "$ZSH_CUSTOM/plugins/pwp"
+else
+    echo "[✓] pwp already installed."
+fi
+
+# stow link dotfiles
 for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
     echo "[+] Folder :: $folder"
     stow --ignore=README.md --ignore=LICENSE \
